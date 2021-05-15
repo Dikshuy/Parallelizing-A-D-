633e9fb6f5c96d8c601efe022b8ba1e0e010bb9f
Dikshuy <dikshant1212hsr@gmail.com>
2021-05-15 12:39:35 +0530

    updated serial a star

--- astar.cpp
+++ astar.cpp
@@ -10,29 +10,20 @@ using namespace std;
 #define ROW 60
 #define COL 60
 
-const int n=10; // horizontal size of the map
-const int m=10; // vertical size size of the map
-static int map[n][m];
-static int closed_nodes_map[n][m]; // map of closed (tried-out) nodes
-static int open_nodes_map[n][m]; // map of open (not-yet-tried) nodes
-static int dir_map[n][m]; // map of directions
-const int dir=4; // number of possible directions to go at any position
-// if dir==4
+static int map[ROW][COL];
+static int closed_nodes_map[ROW][COL]; // map of closed (tried-out) nodes
+static int open_nodes_map[ROW][COL]; // map of open (not-yet-tried) nodes
+static int dir_map[ROW][COL]; // map of directions
+const int dir=4; 
 static int dx[dir]={1, 0, -1, 0};
 static int dy[dir]={0, 1, 0, -1};
-// if dir==8
-// static int dx[dir]={1, 1, 0, -1, -1, -1, 0, 1};
-// static int dy[dir]={0, 1, 1, 1, 0, -1, -1, -1};
 
 class node
 {
-    // current position
     int xPos;
     int yPos;
-    // total distance already travelled to reach the node
     int level;
-    // priority=level+remaining distance estimate
-    int priority;  // smaller: higher priority
+    int priority;
 
     public:
         node(int xp, int yp, int d, int p) 
@@ -45,75 +36,58 @@ class node
 
         void updatePriority(const int & xDest, const int & yDest)
         {
-             priority=level+estimate(xDest, yDest)*10; //A*
+             priority=level+estimate(xDest, yDest)*10; //f=g+h
         }
 
-        // give better priority to going strait instead of diagonally
-        void nextLevel(const int & i) // i: direction
+        void nextLevel(const int & i)
         {
-             level+=(dir==8?(i%2==0?10:14):10);
+             level+=10;
         }
         
-        // Estimation function for the remaining distance to the goal.
         const int & estimate(const int & xDest, const int & yDest) const
         {
             static int xd, yd, d;
             xd=xDest-xPos;
             yd=yDest-yPos;         
 
-            // Euclidian Distance
             d=static_cast<int>(sqrt(xd*xd+yd*yd));
-
-            // Manhattan distance
-            //d=abs(xd)+abs(yd);
-            
-            // Chebyshev distance
-            //d=max(abs(xd), abs(yd));
-
             return(d);
         }
 };
 
-// Determine priority (in the priority queue)
 bool operator<(const node & a, const node & b)
 {
   return a.getPriority() > b.getPriority();
 }
 
-// A-star algorithm.
-// The route returned is a string of direction digits.
-string pathFind( const int & xStart, const int & yStart, 
-                 const int & xFinish, const int & yFinish )
+string pathFind( const int & xStart, const int & yStart, const int & xFinish, const int & yFinish )
 {
-    static priority_queue<node> pq[2]; // list of open (not-yet-tried) nodes
-    static int pqi; // pq index
+    static priority_queue<node> pq[2]; 
+    static int pqi;
     static node* n0;
     static node* m0;
     static int i, j, x, y, xdx, ydy;
     static char c;
     pqi=0;
 
-    // reset the node maps
-    for(y=0;y<m;y++)
+    for(y=0;y<COL;y++)
     {
-        for(x=0;x<n;x++)
+        for(x=0;x<ROW;x++)
         {
             closed_nodes_map[x][y]=0;
             open_nodes_map[x][y]=0;
         }
     }
 
-    // create the start node and push into list of open nodes
     n0=new node(xStart, yStart, 0, 0);
     n0->updatePriority(xFinish, yFinish);
     pq[pqi].push(*n0);
-    open_nodes_map[x][y]=n0->getPriority(); // mark it on the open nodes map
+    open_nodes_map[x][y]=n0->getPriority(); 
 
     // A* search
     while(!pq[pqi].empty())
     {
-        // get the current node w/ the highest priority
-        // from the list of open nodes
+        // get the current node w/ the highest priorityvfrom the list of open nodes
         n0=new node( pq[pqi].top().getxPos(), pq[pqi].top().getyPos(), 
                      pq[pqi].top().getLevel(), pq[pqi].top().getPriority());
 
@@ -125,11 +99,9 @@ string pathFind( const int & xStart, const int & yStart,
         closed_nodes_map[x][y]=1;
 
         // quit searching when the goal state is reached
-        //if((*n0).estimate(xFinish, yFinish) == 0)
         if(x==xFinish && y==yFinish) 
         {
-            // generate the path from finish to start
-            // by following the directions
+            // generate the path from finish to start by following the directions
             string path="";
             while(!(x==xStart && y==yStart))
             {
@@ -139,56 +111,35 @@ string pathFind( const int & xStart, const int & yStart,
                 x+=dx[j];
                 y+=dy[j];
             }
-
-            // garbage collection
             delete n0;
-            // empty the leftover nodes
             while(!pq[pqi].empty()) pq[pqi].pop();           
             return path;
         }
 
-        // generate moves (child nodes) in all possible directions
         for(i=0;i<dir;i++)
         {
             xdx=x+dx[i]; ydy=y+dy[i];
-
-            if(!(xdx<0 || xdx>n-1 || ydy<0 || ydy>m-1 || map[xdx][ydy]==1 
-                || closed_nodes_map[xdx][ydy]==1))
+            if(!(xdx<0 || xdx>ROW-1 || ydy<0 || ydy>COL-1 || map[xdx][ydy]==1 || closed_nodes_map[xdx][ydy]==1))
             {
-                // generate a child node
-                m0=new node( xdx, ydy, n0->getLevel(), 
-                             n0->getPriority());
+                m0=new node( xdx, ydy, n0->getLevel(), n0->getPriority());
                 m0->nextLevel(i);
                 m0->updatePriority(xFinish, yFinish);
-
-                // if it is not in the open list then add into that
                 if(open_nodes_map[xdx][ydy]==0)
                 {
                     open_nodes_map[xdx][ydy]=m0->getPriority();
                     pq[pqi].push(*m0);
-                    // mark its parent node direction
                     dir_map[xdx][ydy]=(i+dir/2)%dir;
                 }
                 else if(open_nodes_map[xdx][ydy]>m0->getPriority())
                 {
-                    // update the priority info
                     open_nodes_map[xdx][ydy]=m0->getPriority();
-                    // update the parent direction info
                     dir_map[xdx][ydy]=(i+dir/2)%dir;
-
-                    // replace the node
-                    // by emptying one pq to the other one
-                    // except the node to be replaced will be ignored
-                    // and the new node will be pushed in instead
-                    while(!(pq[pqi].top().getxPos()==xdx && 
-                           pq[pqi].top().getyPos()==ydy))
+                    while(!(pq[pqi].top().getxPos()==xdx && pq[pqi].top().getyPos()==ydy))
                     {                
                         pq[1-pqi].push(pq[pqi].top());
                         pq[pqi].pop();       
                     }
-                    pq[pqi].pop(); // remove the wanted node
-                    
-                    // empty the larger size pq to the smaller one
+                    pq[pqi].pop();  
                     if(pq[pqi].size()>pq[1-pqi].size()) pqi=1-pqi;
                     while(!pq[pqi].empty())
                     {                
@@ -196,20 +147,17 @@ string pathFind( const int & xStart, const int & yStart,
                         pq[pqi].pop();       
                     }
                     pqi=1-pqi;
-                    pq[pqi].push(*m0); // add the better node instead
+                    pq[pqi].push(*m0); 
                 }
-                else delete m0; // garbage collection
+                else delete m0;
             }
         }
-        delete n0; // garbage collection
+        delete n0; 
     }
-    return ""; // no route found
+    return "";
 }
 
 int main(){
-      
-    // time_t t;
-    // srand(time(NULL));	
 	fstream maps;
 	fstream position;
 	ofstream myfile;
@@ -221,8 +169,8 @@ int main(){
 
 	maps >> Xdimension;
 	maps >> Ydimension;
-	int height = stoi(Xdimension);
-	int width = stoi(Ydimension);
+	int height = stoi(Xdimension); //height = COL
+	int width = stoi(Ydimension);  //width = ROW
 
 	int start_x;
     int start_y;
@@ -231,7 +179,6 @@ int main(){
 	vector<int> solution_path_x;
 	vector<int> solution_path_y;
  
-	
 	position>>start_x;
 	position>>start_y;
 	position>>dest_x;
@@ -249,15 +196,15 @@ int main(){
 		}
 	}
 	cout<<"************************* generated map *************************"<<endl;
-	for(int j=0;j<m;j++){
-		for(int i=0;i<n;i++){
+	for(int j=0;j<COL;j++){
+		for(int i=0;i<ROW;i++){
 			cout<<map[i][j]<<" ";
 		}
 		cout<<endl;
 	}
 	cout<<endl;
 	
-	if(inp_count < n*m){
+	if(inp_count < ROW*COL){
 		cout<<"There is some problem with the input file"<<endl;
 	}
 	else{
@@ -269,8 +216,9 @@ int main(){
 		cout<<endl;
 
 		string route = pathFind(start_x, start_y, dest_x, dest_y);
+		cout<<"Directions: 0->Right, 1->South, 3->Left, 4-> North"<<endl;
 		cout<<"**************************************"<<endl;
-		cout<< route;
+		cout<< route<<endl;
 		cout<<"**************************************"<<endl;
 		cout<<endl;
 		if(route.length()>0){
@@ -289,16 +237,16 @@ int main(){
 	        map[x][y]=2;
 	    }
 
-	    for (int j=0;j<m;j++){
-	    	for (int i=0; i<n;i++){
+	    for (int j=0;j<COL;j++){
+	    	for (int i=0; i<ROW;i++){
 	    		cout<<map[i][j]<<" ";
 	    	}
         cout<<endl;
 	    }
 
 		myfile.open ("solution.txt");
-		for (int i=0; i<height; i++){
-			for(int j=0; j<width; j++){
+		for (int j=0; j<COL; j++){
+			for(int i=0; i<ROW; i++){
 				if (map[i][j]==2){
 					solution_path_x.push_back(i);
 					solution_path_y.push_back(j);
@@ -306,13 +254,13 @@ int main(){
 			}
 		}
   		for(int k=0; k<solution_path_x.size(); k++){
-  			cout<<solution_path_x[k]<<" ";
+  			// cout<<solution_path_x[k]<<" ";
 			myfile <<solution_path_x[k]<<" ";
 		}
 		cout<<endl;
 		myfile << "\n";
 		for(int k=0; k<solution_path_y.size(); k++){
-			cout<<solution_path_y[k]<<" ";
+			// cout<<solution_path_y[k]<<" ";
 			myfile <<solution_path_y[k]<<" ";
 		}
 		myfile << "\n";
